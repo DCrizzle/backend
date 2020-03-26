73b532cfc104fb128ac98ace6ca69b925e4c7ed3
forstmeier <john@shipyardapp.com>
2020-03-25 21:58:53 -0400

    Initial dependency wrapping/test suite

--- app/db.go
+++ app/db.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/dgraph-io/dgo/v2"
@@ -11,7 +12,16 @@ import (
 const (
 	errNewClient = "new client error"
 	errNewDB     = "new db instance error"
+	errMutate    = "mutate execution error"
 )
+
+type transaction interface {
+	Mutate(context.Context, *api.Mutation) (*api.Response, error)
+}
+
+type dgraph interface {
+	transaction() transaction
+}
 
 func newClient(target string) (*dgo.Dgraph, error) {
 	conn, err := grpc.Dial(target, grpc.WithInsecure())
@@ -24,25 +34,47 @@ func newClient(target string) (*dgo.Dgraph, error) {
 	), nil
 }
 
+type dg struct {
+	client *dgo.Dgraph
+}
+
+func (dg *dg) transaction() transaction {
+	return dg.client.NewTxn()
+}
+
 type db struct {
-	dgraph *dgo.Dgraph
+	dgraph dgraph
 }
 
 func newDB() (*db, error) {
 	client, err := newClient("localhost:9080")
 	if err != nil {
-		return nil, fmt.Errorf("%s: %s", errNewDB, err.Error())
+		return nil, err
 	}
 
 	return &db{
-		dgraph: client,
+		dgraph: &dg{
+			client: client,
+		},
 	}, nil
 }
 
-func (db *db) query() {
-	// read
+func (db *db) mutate(input []byte) error {
+	mu := &api.Mutation{
+		CommitNow: true,
+	}
+
+	ctx := context.Background()
+	mu.SetJson = input
+	resp, err := db.dgraph.transaction().Mutate(ctx, mu)
+	if err != nil {
+		return fmt.Errorf("%s: %s", errMutate, err.Error())
+	}
+	_ = resp
+
+	return nil
 }
 
-func (db *db) mutate() {
-	// create/update/delete
+func (db *db) query(input []byte) (interface{}, error) {
+	return nil, nil
 }
--- app/db_test.go
+++ app/db_test.go
@@ -1,7 +1,11 @@
 package app
 
 import (
+	"context"
+	"errors"
 	"testing"
+
+	"github.com/dgraph-io/dgo/v2/protos/api"
 )
 
 func Test_newClient(t *testing.T) {
@@ -15,5 +19,62 @@ func Test_new(t *testing.T) {
 	db, err := newDB()
 	if db == nil || err != nil {
 		t.Errorf("description: error creating db, db: %+v, error: %s", db, err.Error())
+	}
+}
+
+type mockTransaction struct {
+	mutateResp *api.Response
+	mutateErr  error
+}
+
+func (mt *mockTransaction) Mutate(context.Context, *api.Mutation) (*api.Response, error) {
+	return mt.mutateResp, mt.mutateErr
+}
+
+type mockDgraph struct {
+	mockTransaction *mockTransaction
+}
+
+func (md *mockDgraph) transaction() transaction {
+	return md.mockTransaction
+}
+
+func Test_mutate(t *testing.T) {
+	tests := []struct {
+		desc       string
+		mutateResp *api.Response
+		mutateErr  error
+		err        error
+	}{
+		{
+			desc:       "dgraph mutate method error",
+			mutateResp: nil,
+			mutateErr:  errors.New("mock mutate error"),
+			err:        errors.New("mutate execution error: mock mutate error"),
+		},
+		{
+			desc:       "successful invocation",
+			mutateResp: &api.Response{},
+			mutateErr:  nil,
+			err:        nil,
+		},
+	}
+
+	for _, test := range tests {
+		specimen := []byte(`{"type":"blood"}`)
+
+		db := &db{
+			dgraph: &mockDgraph{
+				mockTransaction: &mockTransaction{
+					mutateResp: test.mutateResp,
+					mutateErr:  test.mutateErr,
+				},
+			},
+		}
+
+		err := db.mutate(specimen)
+		if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("description: %s, expected: %v, received: %v", test.desc, test.err, err)
+		}
 	}
 }
