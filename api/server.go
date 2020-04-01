a0c12e22479a4c7e2cd854d2aad37b91605f7b2f
forstmeier <john@shipyardapp.com>
2020-03-31 22:58:38 -0400

    Add secure middleware/test suite

--- api/server.go
+++ api/server.go
@@ -11,21 +11,28 @@ import (
 	"github.com/forstmeier/tbd/app"
 )
 
-const (
-	errNewDB = "error calling new db function"
-)
+const errNewDB = "error calling new db function"
 
-type apiHandler func(db app.Database) func(w http.ResponseWriter, r *http.Request)
+type helper struct {
+	db app.Database
+}
 
-func contextWrap(handler func(db app.Database) func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
-	// outline:
-	// [ ] create context
-	// [ ] validate user
-	// [ ] insert into context with values
-	// - [ ] handlers: handleOrg, handleMutation, handleQuery
-	// [ ] pass into received handler
-	// [ ] return/invoke handler
-})
+func (h *helper) secure(hdlr http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		content := fmt.Sprintf(`{"example": "%s"}`, token) // NOTE: this will be updated
+		query := &app.Query{
+			Content: &content,
+		}
+
+		resp, err := h.db.Query(query)
+		if resp == nil || err != nil {
+			http.Error(w, "forbidden", http.StatusForbidden)
+		}
+
+		hdlr.ServeHTTP(w, r)
+	})
+}
 
 func handleOrg(db app.Database) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -81,12 +88,21 @@ func newServer(addr string) (*server, error) {
 
 	router := mux.NewRouter()
 
+	// outline:
+	// [ ] add non-auth handlers here
+
 	subrouter := router.Host(addr).Subrouter()
 
 	db, err := app.NewDB(addr)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", errNewDB, err)
 	}
+
+	h := &helper{
+		db: db,
+	}
+
+	subrouter.Use(h.secure)
 
 	// NOTE: probably refactor these paths/subrouters
 	subrouter.HandleFunc("/org/{id}", handleOrg(db)).Methods("GET")
--- api/server_test.go
+++ api/server_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,6 +26,63 @@ func (db *testDB) Mutate(input *app.Mutation) error {
 
 func (db *testDB) Query(input *app.Query) ([]byte, error) {
 	return db.respQuery, db.errQuery
+}
+
+type mockHandle struct {
+	w http.ResponseWriter
+	r *http.Request
+}
+
+func (mh *mockHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	mh.w = w
+	mh.r = r
+}
+
+func Test_secure(t *testing.T) {
+	tests := []struct {
+		desc      string
+		respQuery []byte
+		errQuery  error
+		code      int
+	}{
+		{
+			desc:      "token not in database",
+			respQuery: nil,
+			errQuery:  errors.New("mock query error"),
+			code:      403,
+		},
+		{
+			desc:      "successful invocation",
+			respQuery: []byte(`"test":"data"`),
+			errQuery:  nil,
+			code:      200,
+		},
+	}
+
+	for _, test := range tests {
+		h := &helper{
+			db: &testDB{
+				respQuery: test.respQuery,
+				errQuery:  test.errQuery,
+			},
+		}
+
+		mh := &mockHandle{}
+		hdlr := h.secure(mh)
+
+		req, err := http.NewRequest("GET", "/user/123", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rec := httptest.NewRecorder()
+
+		hdlr.ServeHTTP(rec, req)
+
+		if rec.Code != test.code {
+			t.Errorf("description: %s, received: %d, expected: %d", test.desc, rec.Code, test.code)
+		}
+	}
 }
 
 func Test_handleOrg(t *testing.T) {
