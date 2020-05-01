5664e82d0729814f4ede5926cceaf01acf85bc2d
forstmeier <john@shipyardapp.com>
2020-04-30 21:48:21 -0400

    Add server/middleware outlines

--- backend/server.go
+++ backend/server.go
@@ -1,1 +1,60 @@
 package backend
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {}
+
+func NewServer() *Server {
+
+	router := mux.NewRouter()
+
+	router.Use(middleware)
+
+	// outline:
+	// [x] declare mux router
+	// [ ] add authentication middleware
+	// [ ] add graphql handler
+	// [ ] configure/add http server
+	// [ ] return server pointer
+
+	return &Server{}
+}
+
+func middleware(client dgrapher) func(http.Handler) http.Handler {
+
+	return func(next http.Handler) http.Handler {
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+
+
+
+
+		})
+
+	}
+
+	// outline:
+	// [ ] return wrapped handler function
+	// [ ] retrieve token from request
+	// [ ] validate/parse token
+	// [ ] query database with dgraph client
+	// [ ] hydrate full user object
+	// [ ] add user object to request context
+	// [ ] server request with context
+
+}
+
+type dgrapher interface {
+// 	Query(query string, variables map[string]string) ([]byte, error)
+}
+
+// type dgraph struct{}
+//
+// func (d *dgraph) Query(query string, variables map[string]string) ([]byte, error) {
+//
+// }
--- backend/server_test.go
+++ backend/server_test.go
@@ -1,1 +1,15 @@
 package backend
+
+import "testing"
+
+func TestNewServer(t *testing.T) {
+	s := NewServer()
+
+	if s == nil {
+		t.Error("nil received creating new server")
+	}
+}
+
+func Test_middleware(t *testing.T) {
+	h := middleware()
+}
