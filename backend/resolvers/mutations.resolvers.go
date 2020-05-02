321b35a5a223c49304cb4464b4d7a86a68ebb293
forstmeier <john@shipyardapp.com>
2020-05-01 22:56:47 -0400

    Add initial/unimplemented generated resources

---
+++ backend/resolvers/mutations.resolvers.go
@@ -0,0 +1,20 @@
+package resolvers
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/forstmeier/tbd/backend/generated"
+)
+
+func (r *mutationResolver) CreateUser(ctx context.Context, input *generated.UserInput) (*generated.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+type mutationResolver struct{ *Resolver }
