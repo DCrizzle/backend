f2f268c4928012ea9494684de3c7975931564abf
forstmeier <john@shipyardapp.com>
2020-05-08 23:54:44 -0400

    Complete initial handler logic/mutation test suite

--- backend/server.go
+++ backend/server.go
@@ -1,13 +1,24 @@
 package backend
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Server struct {}
+const (
+	errNoPOSTRequestBody   = "no body received in payload"
+	errParsingPOSTJSONBody = "error parsing json body"
+)
 
+// Server hosts the backend server with login/logout and GraphQL endpoints.
+type Server struct{}
+
+// NewServer generates a pointer to an inactive Server instance.
 func NewServer(gql graphql) *Server {
 
 	router := mux.NewRouter()
@@ -38,7 +49,63 @@ func middleware(http.Handler) http.Handler {
 
 }
 
-type graphql interface {
-	query(query string, variables, headers map[string]string) io.ReadCloser
-	mutation(mutation string, variables, headers map[string]string) io.ReadCloser
+func graphQLHandler(gql graphql) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var output io.ReadCloser
+
+		if r.Method == http.MethodPost {
+			mut := mutation{}
+			if r.Body == nil {
+				http.Error(w, fmt.Sprintf(`{"message":"%s"}`, errNoPOSTRequestBody), http.StatusBadRequest)
+				return
+			}
+
+			err := json.NewDecoder(r.Body).Decode(&mut)
+			if err != nil {
+				http.Error(w, fmt.Sprintf(`{"message":"%s"}`, errParsingPOSTJSONBody), http.StatusBadRequest)
+				return
+			}
+
+			mutationResponse := gql.mutation(mut.mutation, mut.variables, r.Header)
+			output = mutationResponse
+
+		} else if r.Method == http.MethodGet {
+			params := r.URL.Query()
+			queryParam, ok := params["query"]
+			if !ok {
+				http.Error(w, "query parameter not received", http.StatusBadRequest)
+				return
+			}
+
+			var variablesParam map[string]string
+			variablesString, ok := params["variables"]
+			if ok {
+				err := json.Unmarshal([]byte(variablesString[0]), variablesParam)
+				if err != nil {
+					http.Error(w, "error parsing query url variable", http.StatusBadRequest)
+					return
+				}
+			}
+
+			queryResponse := gql.query(queryParam[0], variablesParam, r.Header)
+			output = queryResponse
+
+		}
+
+		buffer := new(bytes.Buffer)
+		buffer.ReadFrom(output)
+		fmt.Fprint(w, buffer.String())
+	})
 }
+
+type mutation struct {
+	mutation  string            `json:"query"`
+	variables map[string]string `json:"variables"`
+}
+
+type graphql interface {
+	mutation(mutation string, variables map[string]string, headers map[string][]string) io.ReadCloser
+	query(query string, variables map[string]string, headers map[string][]string) io.ReadCloser
+}
+
+// todo: add implementation of graphql interface utilizing http client for graphql
--- backend/server_test.go
+++ backend/server_test.go
@@ -1,15 +1,107 @@
 package backend
 
-import "testing"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type mockGraphQL struct {
+	mutationResponse io.ReadCloser
+	queryResponse    io.ReadCloser
+}
+
+func (mgql *mockGraphQL) mutation(mutation string, variables map[string]string, headers map[string][]string) io.ReadCloser {
+	return mgql.mutationResponse
+}
+
+func (mgql *mockGraphQL) query(query string, variables map[string]string, headers map[string][]string) io.ReadCloser {
+	return mgql.queryResponse
+}
 
 func TestNewServer(t *testing.T) {
-	s := NewServer()
+	mgql := &mockGraphQL{}
+
+	s := NewServer(mgql)
 
 	if s == nil {
 		t.Error("nil received creating new server")
 	}
 }
 
+func Test_graphQLHandler(t *testing.T) {
+	tests := []struct {
+		description      string
+		requestMethod    string
+		requestBody      io.Reader
+		mutationResponse io.ReadCloser
+		queryResponse    io.ReadCloser
+		status           int
+		body             string
+	}{
+		{
+			description:      "no json body received in post request",
+			requestMethod:    http.MethodPost,
+			requestBody:      nil,
+			mutationResponse: nil,
+			queryResponse:    nil,
+			status:           http.StatusBadRequest,
+			body:             fmt.Sprintf(`{"message":"%s"}`, errNoPOSTRequestBody) + "\n",
+		},
+		{
+			description:      "non-json body received in post request",
+			requestMethod:    http.MethodPost,
+			requestBody:      strings.NewReader("---------"),
+			mutationResponse: nil,
+			queryResponse:    nil,
+			status:           http.StatusBadRequest,
+			body:             fmt.Sprintf(`{"message":"%s"}`, errParsingPOSTJSONBody) + "\n",
+		},
+		{
+			description:      "successful mutation post request invocation",
+			requestMethod:    http.MethodPost,
+			requestBody:      strings.NewReader(`{"query":"test query","variables":{"key":"value"}}`),
+			mutationResponse: ioutil.NopCloser(strings.NewReader(`{"data":{"key":"value"}}`)),
+			queryResponse:    nil,
+			status:           http.StatusOK,
+			body:             `{"data":{"key":"value"}}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			mgql := &mockGraphQL{
+				mutationResponse: test.mutationResponse,
+				queryResponse:    test.queryResponse,
+			}
+
+			req, err := http.NewRequest(test.requestMethod, "/graphql", test.requestBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rec := httptest.NewRecorder()
+
+			handler := http.HandlerFunc(graphQLHandler(mgql))
+
+			handler.ServeHTTP(rec, req)
+
+			if status := rec.Code; status != test.status {
+				t.Errorf("status received: %d, expected: %d", status, test.status)
+			}
+
+			received := rec.Body.String()
+			if received != test.body {
+				t.Errorf("body received: %s, expected: %s", received, test.body)
+			}
+		})
+	}
+}
+
 func Test_middleware(t *testing.T) {
-	h := middleware()
+
 }
