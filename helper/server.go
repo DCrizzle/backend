75c3dffb7a2f1a042c10c544a993a09e736f5820
forstmeier <john@shipyardapp.com>
2020-08-05 08:54:10 -0400

    Refactor/cleanup server resources

--- helper/server.go
+++ helper/server.go
@@ -2,64 +2,17 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
-type responseJSON struct {
-	AccessToken string `json:"access_token"`
-}
-
-func getAuth0ManagementAPIToken(url string) (string, error) {
-	c, err := readConfig()
-	if err != nil {
-		return "", err
-	}
-
-	payloadString := fmt.Sprintf(
-		"grant_type=client_credentials&client_id=%s&client_secret=%s&audience=%s",
-		c.Auth0.ClientID,
-		c.Auth0.ClientSecret,
-		c.Auth0.Audience,
-	)
-
-	payload := strings.NewReader(payloadString)
-	req, err := http.NewRequest("POST", url, payload)
-	if err != nil {
-		return "", err
-	}
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer res.Body.Close()
-
-	resBytes, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-
-	resJSON := responseJSON{}
-	if err := json.Unmarshal(resBytes, &resJSON); err != nil {
-		return "", err
-	}
-
-	return resJSON.AccessToken, nil
-}
-
 type server struct {
-	auth0Token string
 	httpServer *http.Server
 }
 
-func newServer(auth0Token string, usersHandler http.HandlerFunc) *server {
+func newServer(usersHandler http.HandlerFunc) *server {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/auth0").Subrouter()
 	subrouter.HandleFunc("/users", usersHandler)
@@ -72,7 +25,6 @@ func newServer(auth0Token string, usersHandler http.HandlerFunc) *server {
 	}
 
 	return &server{
-		auth0Token: auth0Token,
 		httpServer: helperServer,
 	}
 }
@@ -83,17 +35,4 @@ func (s *server) start() {
 
 func (s *server) stop(ctx context.Context) {
 	s.httpServer.Shutdown(ctx)
-}
-
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-
-	// outline:
-	// [ ] retrieve secret code
-	// - [ ] validate against stored code
-	// [ ] parse received data
-	// [ ] construct auth0 request based on method
-	// - [ ] post method
-	// - [ ] patch method
-	// - [ ] delete method
-
 }
--- helper/server_test.go
+++ helper/server_test.go
@@ -1,61 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-func Test_getAuth0ManagementAPIToken(t *testing.T) {
-	tests := []struct {
-		description string
-		response    string
-		token       string
-		err         string
-	}{
-		{
-			description: "successful helper function invocation",
-			response:    `{"access_token": "test-token"}`,
-			token:       "test-token",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.description, func(t *testing.T) {
-			mux := http.NewServeMux()
-			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprint(w, test.response)
-			})
-
-			server := httptest.NewServer(mux)
-			token, err := getAuth0ManagementAPIToken(server.URL)
-
-			if token != test.token {
-				t.Errorf("token received: %s, expected: %s", token, test.token)
-			}
-
-			if err != nil && test.err != "" {
-				t.Errorf("error received: %s, expected: %s", err.Error(), test.err)
-			}
-		})
-	}
-}
-
 func Test_newServer(t *testing.T) {
 	testHandler := func(w http.ResponseWriter, r *http.Request) {}
-	server := newServer("TOKEN", testHandler)
+	server := newServer(testHandler)
 	if server == nil {
 		t.Error("error creating server")
 	}
-}
-
-func Test_usersHandler(t *testing.T) {
-
-	// outline:
-	// [ ] create mock auth0 server/handlers
-	// [ ] pass in mock client
-	// - [ ] include url to mock auth0 server
-	// [ ] test scenarios/data retrieved
-
 }
