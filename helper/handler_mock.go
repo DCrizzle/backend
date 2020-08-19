4ece0418f87a6b3eae42b5ce91521b63a88938e9
forstmeier <john@shipyardapp.com>
2020-08-18 22:06:14 -0400

    Expand helper mock handler function

--- helper/handler_mock.go
+++ helper/handler_mock.go
@@ -3,12 +3,33 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func usersHandler(secret, token, url string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `{"message": "success"}`)
-	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var dgraphReqJSON dgraphRequest
+		if err := json.NewDecoder(r.Body).Decode(&dgraphReqJSON); err != nil {
+			http.Error(w, errIncorrectRequestBody, http.StatusBadRequest)
+			return
+		}
+
+		auth0ID := ""
+		if r.Method == http.MethodPost {
+			id := uuid.New().String()
+			hexID := hex.EncodeToString([]byte(id))
+			auth0ID = "auth0|" + hexID
+		} else if r.Method == http.MethodPatch {
+			auth0ID = dgraphReqJSON.UserID
+		} else if r.Method == http.MethodDelete {
+			auth0ID = dgraphReqJSON.UserID
+		}
+
+		fmt.Fprintf(w, fmt.Sprintf(`{"message": "success", "user_id": "%s"}`, auth0ID))
+	})
 }
