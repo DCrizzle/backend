d22248ff6d4c4ad0b284dfb569ad29dccd66d0be
forstmeier <john@shipyardapp.com>
2020-10-03 22:45:45 -0400

    Adjust error file message contents

--- custom/handlers/errors.go
+++ custom/handlers/errors.go
@@ -8,19 +8,19 @@ const (
 	// ErrIncorrectHTTPMethod is an unsupported HTTP method was received
 	ErrIncorrectHTTPMethod = "unsupported http method received"
 	// ErrMarshallingCreateJSON is an error marshalling create Auth0 user JSON
-	ErrMarshallingCreateJSON = "error marshalling create json"
+	ErrMarshallingCreateJSON = "error marshalling create user json"
 	// ErrMarshallingUpdateJSON is an error marshalling update Auth0 user JSON
-	ErrMarshallingUpdateJSON = "error marshalling update json"
+	ErrMarshallingUpdateJSON = "error marshalling update user json"
 	// ErrCreatingAuth0Request is an error creating an Auth0 HTTP request
-	ErrCreatingAuth0Request = "error creating auth0 request"
+	ErrCreatingAuth0Request = "error creating auth0 user request"
 	// ErrExecutingAuth0Request is an error executing an Auth0 HTTP request
-	ErrExecutingAuth0Request = "error executing auth0 request"
+	ErrExecutingAuth0Request = "error executing auth0 user request"
 	// ErrUnmarshallingResponseBody is an error unmarshalling an Auth0 response
-	ErrUnmarshallingResponseBody = "incorrect response body received"
+	ErrUnmarshallingResponseBody = "error unmarshalling auth0 response body"
 	// ErrDgraphMutation is an error executing the custom server Dgraph mutation
 	ErrDgraphMutation = "error executing dgraph mutation"
 	// ErrMarshallingDgraphJSON is an error marshalling a Dgraph response
-	ErrMarshallingDgraphJSON = "error marshalling dgraph json"
+	ErrMarshallingDgraphJSON = "error marshalling dgraph response json"
 	// ErrClassifyingEntities is an error invoking the classifier
 	ErrClassifyingEntities = "error classifying entities"
 )
